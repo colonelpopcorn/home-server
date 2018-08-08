96712c659c6e1757524b37dff5a51b1cc63719a4
Jonathan Ling <jonathan.ling@aptean.com>
2018-08-08 15:05:53 -0400

    Add powershell load env script to python script.

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,10 +7,25 @@ def setup_dirs():
       sudo mkdir -pv ./volumes/app/mattermost/{data,logs,config} \
       chown -R 2000:2000 ./volumes/app/mattermost/
     """
-    subprocess.call([dir_script])
+    subprocess.call(['sh', '-c', dir_script])
+
+def load_env():
+    ps_script = """
+        foreach($line in Get-Content $args[0]) {
+            $envSplit = $line.Split("="); if (!$envSplit[0].startsWith("#")) {
+                [System.Environment]::SetEnvironmentVariable($envSplit[0], $envSplit[1], [EnvironmentVariableTarget]::Process)
+            }
+        }
+    """
 
 def run_all():
     # Add all your utility functions here.
+    setup_dirs()
+    load_env()
+    return True
+
+# Jinja template the toml config for reusability
+def prep_toml():
     return True
 
 if __name__ == '__main__':
