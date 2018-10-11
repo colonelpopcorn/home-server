46f0a64c36b1286964cfabf67862bb9dbde48975
Jonathan Ling <jonathan.ling@aptean.com>
2018-10-11 11:20:26 -0400

    Whitespace changes.

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,8 +4,8 @@ import subprocess
 
 def setup_dirs():
     dir_script = """
-      sudo mkdir -pv ./volumes/app/mattermost/{data,logs,config} \
-      chown -R 2000:2000 ./volumes/app/mattermost/
+    sudo mkdir -pv ./volumes/app/mattermost/{data,logs,config} \
+    chown -R 2000:2000 ./volumes/app/mattermost/
     """
     subprocess.call(['sh', '-c', dir_script])
 
@@ -31,4 +31,4 @@ def prep_toml():
 if __name__ == '__main__':
     args = sys.argv
     if len(args) <= 0:
-      run_all()
+    run_all()
